487c060df1296a53f389d4e47a46755c27c49114
Will Collins <williamacollins05@gmail.com>
2026-02-04 14:49:23 -0600

    OpenIddict Basic Implementation
    Basic OpenIddict implementation is added, allowing users to register accounts, log in, and log out.
    
    Solid 10 hours of my life right here.

---
+++ CocktailDbSite.Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CocktailDbSite.Domain.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
--- CocktailDbSite.Infrastructure/DependencyInjection.cs
+++ CocktailDbSite.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,14 @@
 ﻿using CocktailDbSite.Domain.Interfaces;
 using CocktailDbSite.Domain.Services;
+using CocktailDbSite.Domain.Identity;
 using CocktailDbSite.Infrastructure.Data;
 using CocktailDbSite.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
 
 namespace CocktailDbSite.Infrastructure;
 
@@ -12,7 +16,51 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<CocktailDbContext>(o => o.UseNpgsql(configuration["CONNECTION_STRING"]));
+        services.AddDbContext<CocktailDbContext>(o => 
+        {
+            o.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            o.UseOpenIddict();
+        });
+
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false; 
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<CocktailDbContext>()
+        .AddDefaultTokenProviders();
+
+        services.AddOpenIddict()
+            .AddCore(options =>
+            {
+                options.UseEntityFrameworkCore()
+                       .UseDbContext<CocktailDbContext>();
+            })
+            .AddServer(options =>
+            {
+                options.SetTokenEndpointUris("/connect/token");
+                options.AllowPasswordFlow();
+                options.AcceptAnonymousClients(); 
+                options.AddDevelopmentEncryptionCertificate()
+                       .AddDevelopmentSigningCertificate();
+                options.UseAspNetCore()
+                       .EnableTokenEndpointPassthrough()
+                       .EnableAuthorizationEndpointPassthrough()
+                       .EnableEndSessionEndpointPassthrough();
+                options.SetAuthorizationEndpointUris("/connect/authorize")
+                    .SetTokenEndpointUris("/connect/token")
+                    .SetEndSessionEndpointUris("/connect/logout");
+            })
+            .AddValidation(options =>
+            {
+                options.UseLocalServer();
+                options.UseAspNetCore();
+            });
+
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddHttpClient<ICocktailProvider, CocktailDbService>();
         return services;
