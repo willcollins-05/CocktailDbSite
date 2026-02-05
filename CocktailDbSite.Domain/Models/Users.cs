eed5ecab51f7979fdd973925c81e0c57347c76b1
KindredMouse <b2004tl@gmail.com>
2026-02-04 21:20:30 -0600

    added users

---
+++ CocktailDbSite.Domain/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CocktailDbSite.Domain.Models;
+
+
+public class Users
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+}
--- CocktailDbSite.Infrastructure/Data/CocktailDbContext.cs
+++ CocktailDbSite.Infrastructure/Data/CocktailDbContext.cs
@@ -13,7 +13,7 @@ public class CocktailDbContext : IdentityDbContext<ApplicationUser, ApplicationR
     
     // DbSet<T>
     public DbSet<TestTable> TestTables { get; set; }
-
+    public DbSet<Users> Users { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
